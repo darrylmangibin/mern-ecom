66253bcb36ef1377dd2a984544fd71ce5b131e47
darrylmangibin <darrylmangibin@gmail.com>
2021-10-05 21:53:34 +0800

    Add models of ur data

---
+++ back-end/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      require: true,
+      default: false,
+    },
+  },
+  {
+    timetamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
